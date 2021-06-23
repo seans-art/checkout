6db479451c4061ff219b7215127e13123b118c31
alexstouffer <bizkite@hotmail.com>
2021-06-23 13:40:16 -0700

    Update publicPath in Config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: "/", 
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
   devServer: {
     disableHostCheck: true,
     public: process.env.VUE_APP_DEV_SERVER,
