6c7920bd65f7f07dd4071f260998c960b6ca5b0a
alexstouffer <bizkite@hotmail.com>
2021-06-24 16:32:06 -0700

    update order to query param

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/checkout/'
-    : '/',
   devServer: {
     disableHostCheck: true,
     public: process.env.VUE_APP_DEV_SERVER,
