c2737f4f0838360612ae267be1154221622acf90
alexstouffer <bizkite@hotmail.com>
2021-06-24 17:05:10 -0700

    mod deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ echo 'checkout.seanobrien.art' > dist/CNAME
 cp dist/* docs/ -r
 # git init
 git add -A
-git commit -m '$1'
+git commit -m "$1"
 git push # git@github.com:seans-art/checkout.git master
